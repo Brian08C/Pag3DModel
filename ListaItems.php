ea3fbf646bac19eb47ffb888a106edfb5e7eba43
Brian08C <93220753+Brian08C@users.noreply.github.com>
2023-06-23 09:58:04 -0500

    Update ListaItems.php

--- ListaItems.php
+++ ListaItems.php
@@ -6,7 +6,7 @@ $files = glob("",$folder,$mask);
 foreach ($files as $file) 
 {
 	$file_name = basename($file,substr($mask,1));
-	$name = split("-",$file_name);
+	$name = explode("-",$file_name);
 	echo $name[0],",";
 }
 ?>
